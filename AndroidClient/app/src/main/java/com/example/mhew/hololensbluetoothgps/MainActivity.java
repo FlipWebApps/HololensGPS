dce6b8655c1ae10d7974a917ac0b15dcacea2124
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2017-10-14 16:03:48 +0200

    Bug fixes for Android application and improved execution order

--- AndroidClient/app/src/main/java/com/example/mhew/hololensbluetoothgps/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/mhew/hololensbluetoothgps/MainActivity.java
@@ -20,15 +20,19 @@ import android.widget.Toast;
 import com.google.android.gms.maps.MapView;
 
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    TextView textViewTime;
+    TextView textViewLatitude;
+    TextView textViewLongitude;
     MapView mapView;
 
-    BluetoothAdapter mBAdapter;
-    BluetoothManager mBManager;
+    BluetoothAdapter mBluetoothAdapter;
     BluetoothLeAdvertiser mBLEAdvertiser;
     static final int BEACON_ID = 1775;
+    public static final int REQUEST_ENABLE_BT = 1;
 
     LocationManager mLocationManager;
     static final int PERMISSION_RESULT_CODE = 1;
@@ -40,19 +44,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Setup UI References
-        textView = (TextView) findViewById(R.id.textView);
+        textViewTime = (TextView) findViewById(R.id.textViewTime);
+        textViewLatitude = (TextView) findViewById(R.id.textViewLatitude);
+        textViewLongitude = (TextView) findViewById(R.id.textViewLongitude);
         mapView = (MapView) findViewById(R.id.mapView);
 
-        mBManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-        mBAdapter = mBManager.getAdapter();
-        mBLEAdvertiser = mBAdapter.getBluetoothLeAdvertiser();
+        if (savedInstanceState == null) {
+            // Use this check to determine whether BLE is supported on the device. Then you can
+            // selectively disable BLE-related features.Only needed if required=false in manifest e.g.:
+            // <uses-feature android:name="android.hardware.bluetooth_le" android:required="false"/>
+            if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+                Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+                finish();
+            }
 
-        int permissionCheck = ContextCompat.checkSelfPermission((Context)this, Manifest.permission.ACCESS_FINE_LOCATION);
+            // Is Bluetooth supported on this device?
+            mBluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE))
+                    .getAdapter();
+            if (mBluetoothAdapter != null) {
+                // Is Bluetooth turned on?
+                if (mBluetoothAdapter.isEnabled()) {
+                    // Are Bluetooth Advertisements supported on this device?
+                    if (mBluetoothAdapter.isMultipleAdvertisementSupported()) {
+                        // Everything is supported and enabled, so proceed.
+                        mBLEAdvertiser = mBluetoothAdapter.getBluetoothLeAdvertiser();
+                    } else {
+                        // Bluetooth Advertisements are not supported.
+                        Toast.makeText(this, R.string.bt_ads_not_supported, Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                } else {
+                    // Prompt user to turn on Bluetooth (logic continues in onActivityResult()).
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+            } else {
+                Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // only process GPS when activity is running.
+        int permissionCheck = ContextCompat.checkSelfPermission((Context) this, Manifest.permission.ACCESS_FINE_LOCATION);
         if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
             startGPS();
-        }
-        else
-        {
+        } else {
             // No explanation needed, we can request the permission.
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
@@ -61,34 +102,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (mBAdapter == null || !mBAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBtIntent);
-            finish();
-            return;
-        }
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "No LE support on this device", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-        if (!mBAdapter.isMultipleAdvertisementSupported()) {
-            Toast.makeText(this, "No advertising support on this device", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-        startAdvertising();
-    }
-
-    @Override
     protected void onPause() {
         super.onPause();
+        stopGPS();
         stopAdvertising();
     }
 
-    private void startAdvertising() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == RESULT_OK) {
+                    // Bluetooth is now Enabled, are Bluetooth Advertisements supported on
+                    // this device?
+                    if (mBluetoothAdapter.isMultipleAdvertisementSupported()) {
+                        // Everything is supported and enabled, so proceed.
+                        mBLEAdvertiser = mBluetoothAdapter.getBluetoothLeAdvertiser();
+                    } else {
+                        // Bluetooth Advertisements are not supported.
+                        Toast.makeText(this, R.string.bt_ads_not_supported_leaving, Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                } else {
+                    // User declined to enable Bluetooth, exit the app.
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving,
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_RESULT_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    startGPS();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "ACCESS_FINE_LOCATION Denied. Quitting!", Toast.LENGTH_SHORT)
+                            .show();
+                    finish();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void startAdvertising(int manufactureId, byte[] manufactureData) {
         if (mBLEAdvertiser == null) return;
         AdvertiseSettings settings = new AdvertiseSettings.Builder()
                 .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
@@ -97,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
                 .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
                 .build();
         AdvertiseData data = new AdvertiseData.Builder()
-                .addManufacturerData(BEACON_ID, buildGPSPacket())
+                .addManufacturerData(manufactureId, manufactureData)
                 .build();
         mBLEAdvertiser.startAdvertising(settings, data, mAdvertiseCallback);
     }
@@ -107,9 +174,9 @@ public class MainActivity extends AppCompatActivity {
         mBLEAdvertiser.stopAdvertising(mAdvertiseCallback);
     }
 
-    private void restartAdvertising() {
+    private void restartAdvertising(int manufactureId, byte[] manufactureData) {
         stopAdvertising();
-        startAdvertising();
+        startAdvertising(manufactureId, manufactureData);
     }
 
     private AdvertiseCallback mAdvertiseCallback = new AdvertiseCallback() {
@@ -126,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
                 String msg = "Service failed to start: " + errorCode;
                 mHandler.sendMessage(Message.obtain(null, 0, msg));
             } else {
-                restartAdvertising();
+                //restartAdvertising();
             }
         }
     };
@@ -134,52 +201,59 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            /*
-            UI feedback to the user would go here.
-            */
-            Toast.makeText(MainActivity.this, (CharSequence)msg.toString(), Toast.LENGTH_LONG).show();
+            //Toast.makeText(MainActivity.this, new SimpleDateFormat("HH.mm.ss").format(new Date()) +
+            //        ": " + (CharSequence) msg.toString(), Toast.LENGTH_SHORT).show();
         }
     };
 
-    public void startGPS()
-    {
+    //region GPS
+
+    LocationListener listener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            UpdatePosition(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
+
+    public void startGPS() {
         //Execute location service call if user has explicitly granted ACCESS_FINE_LOCATION..
-        mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        LocationListener listener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                UpdatePosition(location);
-            }
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-            @Override
-            public void onProviderEnabled(String provider) {
-            }
-            @Override
-            public void onProviderDisabled(String provider) {
-            }
-        };
-        try
-        {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
             mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
             currentLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            UpdatePosition(currentLocation);
             Log.i("GPS_Receiver", "startGPS: GPS Started..");
+        } catch (SecurityException e) {
         }
-        catch(SecurityException e)
-        {
-        }
-    }
-    public void UpdatePosition(Location location)
-    {
-        currentLocation = location;
-        textView.setText(Double.toString(currentLocation.getLatitude()) + "," + Double.toString(currentLocation.getLongitude()));
-        restartAdvertising();
     }
 
-    private byte[] buildGPSPacket()
-    {
+    public void stopGPS() {
+        if (mLocationManager == null) return;
+        mLocationManager.removeUpdates(listener);
+    }
+
+    public void UpdatePosition(Location location) {
+        currentLocation = location;
+        textViewTime.setText(new SimpleDateFormat("HH.mm.ss").format(new Date()));
+        textViewLatitude.setText(Double.toString(currentLocation.getLatitude()));
+        textViewLongitude.setText(Double.toString(currentLocation.getLongitude()));
+        restartAdvertising(BEACON_ID, buildGPSPacket());
+    }
+
+    private byte[] buildGPSPacket() {
         Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         byte[] packet = new byte[24];
         if(location != null) {
@@ -204,4 +278,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return packet;
     }
+    //endregion GPS
+
 }
